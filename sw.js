b97480f899650d3381a8fdb32a7585727580539f
fayzullo7030 <fayzullo.org@gmail.com>
2026-09-09 10:19:28 +0500

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE="gold-tracker-v1";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.webmanifest","./icon.svg"]))));
-self.addEventListener("fetch",e=>{if(e.request.method==="GET")e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request)))});
-self.addEventListener("message",e=>{if(e.data==="SKIP_WAITING")self.skipWaiting()});
